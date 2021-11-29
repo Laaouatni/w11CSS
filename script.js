4d01dbf3e0ac60b2761e302d81a20e8a17d31fbe
laaouatni <laaouatni.anas@outlook.it>
2021-11-29 22:29:19 +0100

    testing but not work, domani da correggere

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function visible() {
     const startSection = document.getElementById('#w11-start-section');
     const input = document.getElementById('#windows-on-off');
     /* if input checkbox is checked, show the start section */
-    if (input.checked) {
-        startSection.className = 'on';
-        console.log('checked');
-    } else {
-        startSection.className = 'off';
-        console.log('disabled');
-    }
+    /*     if () {
+            startSection.className = 'on';
+            console.log('checked');
+        } else { */
+    startSection.className = 'off';
+    console.log('disabled');
 }
+/* } */
 /* dont working tomorrow i will correct */
