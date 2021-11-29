0f3c7e638ba3833005cbee368f2f3fdc99ef1540
laaouatni <laaouatni.anas@outlook.it>
2021-11-29 22:11:08 +0100

    w11 clone beta con errori

---
+++ script.js
@@ -0,0 +1,13 @@
+function visible() {
+    const startSection = document.getElementById('#w11-start-section');
+    const input = document.getElementById('#windows-on-off');
+    /* if input checkbox is checked, show the start section */
+    if (input.checked) {
+        startSection.className = 'on';
+        console.log('checked');
+    } else {
+        startSection.className = 'off';
+        console.log('disabled');
+    }
+}
+/* dont working tomorrow i will correct */
