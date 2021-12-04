4cf97778829831cbec9a5ee6c2fcc696d0e7324d
laaouatni <laaouatni.anas@outlook.it>
2021-12-04 11:26:59 +0100

    beta con script esterno funzionante -> defer

---
+++ script.js
@@ -0,0 +1,10 @@
+let startContainer = document.querySelector('#w11-start-section');
+let btn = document.querySelector('#windows-div');
+let widgetContainer = document.querySelector('#')
+
+
+
+/* from bottom to top WINDOWS START animation */
+btn.addEventListener("click", function() {
+    startContainer.classList.toggle("on-visible");
+});
